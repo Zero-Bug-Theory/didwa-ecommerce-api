fcf55cc6e11d6c301eae89a2f37046bb7aacf2ae
Akwaboah Emmanuel <starpeaktech@gmail.com>
2026-04-02 14:19:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/debug-db", async (req, res) => {
+  try {
+    const [rows] = await db.query("SHOW TABLES");
+    res.json(rows);
+  } catch (err) {
+    console.error("DB ERROR:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(PORT, "0.0.0.0", () =>
   console.log(`Server running on port ${PORT}`)
 );
