67c17cd76f26b64ed1e63ef1fb7850c001b7759d
Akwaboah Emmanuel <starpeaktech@gmail.com>
2026-04-05 13:57:36 -0700

    Update orderModel.js

--- models/orderModel.js
+++ models/orderModel.js
@@ -10,37 +10,7 @@ const Order = {
   },
 
   getByUserId: (userId, callback) => {
-    try {
-      // 1. Get orders
-      const [orders] = await db.query(
-        "SELECT * FROM orders WHERE user_id = ? ORDER BY id DESC",
-        [userId]
-      );
-  
-      // 2. Attach items to each order
-      for (let order of orders) {
-        const [items] = await db.query(
-          `SELECT oi.*, p.name 
-           FROM order_items oi
-           JOIN products p ON oi.product_id = p.id
-           WHERE oi.order_id = ?`,
-          [order.id]
-        );
-  
-        order.items = items;
-      }
-  
-      // 3. Return
-      res.json({ orders });
-  
-    } catch (err) {
-      console.error("USER ORDERS ERROR:", err);
-      res.status(500).json({
-        message: "Server error",
-        error: err.message
-      });
-    }
-}  
-};
+    db.query("SELECT * FROM orders WHERE user_id = ?", [userId], callback);
+  },
 
 module.exports = Order;
